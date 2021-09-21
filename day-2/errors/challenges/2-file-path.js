9f908354991b81f2547e27f39fadab659b0bd9c1
josephcoder76 <chenstyle@talk21.com>
2021-09-21 15:26:37 +0100

    All the error Challenges and Questions complete

--- day-2/errors/challenges/2-file-path.js
+++ day-2/errors/challenges/2-file-path.js
@@ -1,14 +1,12 @@
 const { check, printGreenMessage, printRedMessage } = require("../../../test-api");
 
-
 const filePath = "/Users/mitch/northcoders/remote_precourse/day_1/1_intro_strings";
 
 // Use .lastIndexOf() to find the index position of the last / forward slash in the filePath string
-let lastForwardSlashIndex = filePath.lastIndexOf('/');
-console.log(lastForwardSlashIndex);
+let lastForwardSlashIndex = filePath.lastIndexOf("/");
 
 // Use .slice() to access the directory at the end of filePath
-let currentDirectory = filePath.slice(lastForwardSlashIndex);
+let currentDirectory = filePath.slice(lastForwardSlashIndex+1);
 
 console.log("currentDirectory is initialised with the name of the directory and the end of the filePath");
 try {
@@ -20,3 +18,4 @@ try {
 
 // Don't change anything below this line :)
 var EXPRESSION_HERE;
+
