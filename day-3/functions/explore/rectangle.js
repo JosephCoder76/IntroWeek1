4774b99788cbcab3ce5a3c2a070bf4c4c4421187
josephcoder76 <chenstyle@talk21.com>
2021-09-22 12:29:59 +0100

    Completed Function explore rectangle.js

--- day-3/functions/explore/rectangle.js
+++ day-3/functions/explore/rectangle.js
@@ -10,4 +10,16 @@
 
 // You don't need to use loops for this one! :)
 
-function makeRectangle() {}
+function makeRectangle(num1, num2) {
+
+//Firstly we use the repeat method to create the three *** and append it with the next line switch
+let threeStars = '*'.repeat(num1) + "\n";
+
+//Now we take the value of threeStars and use th repeat method again for the number of rows and pass num 2 to the repeat method
+let finalString = threeStars = threeStars.repeat(num2)
+
+return finalString;
+
+}
+
+console.log(makeRectangle(3,5));
