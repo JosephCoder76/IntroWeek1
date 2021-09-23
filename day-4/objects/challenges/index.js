51363e4015031d2d31306691780810c8531f42ee
josephcoder76 <chenstyle@talk21.com>
2021-09-23 12:36:06 +0100

    Completed Objects Challenges index.js Day4

--- day-4/objects/challenges/index.js
+++ day-4/objects/challenges/index.js
@@ -2,6 +2,11 @@ const { check, runTest, skipTest } = require("../../../test-api");
 
 // Once you have passed the current test, change skipTest on the following test to runTest so you are able to run it with Node
 // accessObject() should take an object and a key and return the object's property value
+function accessObject(ObjName, val){
+    
+  return ObjName[val];
+}
+
 
 runTest("accessObject() can access a property value using a key", function () {
   check(accessObject).whenCalledWith({ name: "jonny", age: 32 }, "name").returns("jonny");
@@ -9,33 +14,69 @@ runTest("accessObject() can access a property value using a key", function () {
 });
 
 // checkIfPropertyExists() should take an object and a key and return a boolean that indicates whether or not the object has the given keys
+function checkIfPropertyExists(objName, val){
+ 
+   return objName.hasOwnProperty(val)
+ 
+}
 
-skipTest("checkIfPropertyExists() checks if a property exists inside an object", function () {
+runTest("checkIfPropertyExists() checks if a property exists inside an object", function () {
   check(checkIfPropertyExists).whenCalledWith({ name: "jonny", age: 32 }, "name").returns(true);
   check(checkIfPropertyExists).whenCalledWith({ name: "jonny", age: 32 }, "age").returns(true);
   check(checkIfPropertyExists).whenCalledWith({ name: "jonny", age: 32 }, "pets").returns(false);
 });
 
-// createObject() should take a key-value pair (stored in an array) and use it to create an object with a key and a value
+// createObject() should take a key-value pair (stored in an array) 
+//and use it to create an object with a key and a value
+//Refer to Object.fromEntries which you pass an array in to but is must be an array of arrays
+//So if we have just one array to pass in then we must nest the array in [] so our parameter
+//that is passed to the funciton is a single array ["name", "shaq"], so the method needs
+//[["name", "shaq"]]
+function createObject(arr){
+  return Object.fromEntries([arr]);
+}
 
-skipTest("createObject() creates a new object from a key value pair", function () {
+
+runTest("createObject() creates a new object from a key value pair", function () {
   check(createObject).whenCalledWith(["name", "shaq"]).returns({ name: "shaq" });
   check(createObject).whenCalledWith(["fruit", "apple"]).returns({ fruit: "apple" });
   check(createObject).whenCalledWith(["language", "haskell"]).returns({ language: "haskell" });
 });
 
 // countProperties() should take an object and count the number of properties it has
+function countProperties(obj){
+ let count = 0;
+ 
+  for (const el in obj ){
+   count +=1
+  }
+  return count; 
+}
 
-skipTest("countProperties() counts the number of key-value pairs for a given object", function () {
+
+runTest("countProperties() counts the number of key-value pairs for a given object", function () {
   check(countProperties).whenCalledWith({}).returns(0);
   check(countProperties).whenCalledWith({ name: "shaq" }).returns(1);
   check(countProperties).whenCalledWith({ name: "shaq", job: "tutor", city: "Manchester" }).returns(3);
 });
 
-// createArrow() will take a direction string: "left", "right", "up" and "down" and return a corresponding arrow
+// createArrow() will take a direction string: "left", "right", 
+//"up" and "down" and return a corresponding arrow
 // Think of how you can use an object to solve this problem
 
-skipTest("createArrow() will return an arrow pointing in the right direction", function () {
+function createArrow(direction){
+//Create an object with key pair values with the direction as the key label and the arrow as the
+//value
+ const arrowObj = {
+  "right": "→",
+  "left": "←",
+  "up": "↑",
+  "down": "↓"
+ }
+  return arrowObj[direction];
+}
+
+runTest("createArrow() will return an arrow pointing in the right direction", function () {
   check(createArrow).whenCalledWith("right").returns("→");
   check(createArrow).whenCalledWith("left").returns("←");
   check(createArrow).whenCalledWith("up").returns("↑");
@@ -57,7 +98,14 @@ skipTest("createArrow() will return an arrow pointing in the right direction", f
 //    Note - The function does NOT need to return anything.
 //
 
-skipTest("updateVoterAddress() updates the voter's houseNumber", function () {
+    function updateVoterAddress(voterObject){
+   
+     voterObject.name = "Alex";
+     voterObject.age = 39;
+     voterObject.address.houseNumber = 10;
+    }
+
+runTest("updateVoterAddress() updates the voter's houseNumber", function () {
   const voter = {
     name: "Alex",
     age: 39,
@@ -88,7 +136,13 @@ skipTest("updateVoterAddress() updates the voter's houseNumber", function () {
 // Note - this is a good use case of template literals:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 
-skipTest("createUserString() will create a message from an details object", function () {
+    function createUserString(createNorthcoder) {
+ 
+    return `name: ${createNorthcoder.name}, age: ${createNorthcoder.age}, language: ${createNorthcoder.language}`
+    }
+
+
+runTest("createUserString() will create a message from an details object", function () {
   check(createUserString)
     .whenCalledWith({ name: "Mitch", age: 30, language: "Javascript" })
     .returns("name: Mitch, age: 30, language: Javascript");
@@ -106,8 +160,22 @@ skipTest("createUserString() will create a message from an details object", func
 //    '10p: 0
 //  }
 // and should update the coinMachine to reflect any change that is added into the machine
+    function updateCoinMachine(coinMachineObj, coin){
+    //search for the referenced key name passed by coin, for each instance of the key
+    //add one to its value, for th key passed increase the val by 1
+    
+     for (const key in coinMachineObj){
+      if (key === coin){
+        //Remember that if the key is in quotes then you need to use [] notation;
+        coinMachineObj[coin]++;
+      }
+    }
+    return coinMachineObj;
+    }
 
-skipTest("updateCoinMachine() will create a message from an details object", function () {
+
+
+runTest("updateCoinMachine() will create a message from an details object", function () {
   check(updateCoinMachine)
     .whenCalledWith({ "1p": 0, "2p": 0, "5p": 0, "10p": 0 }, "1p")
     .returns({ "1p": 1, "2p": 0, "5p": 0, "10p": 0 });
