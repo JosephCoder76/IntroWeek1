a7b1c1658c190197a777b4dc23a7e4a4e3772233
josephcoder76 <chenstyle@talk21.com>
2021-09-23 10:34:15 +0100

    Completed Day4 the Objects exercises index.js

--- day-4/objects/exercises/index.js
+++ day-4/objects/exercises/index.js
@@ -1,70 +1,73 @@
 const { runTest, skipTest, check } = require("../../../test-api");
-
+//Checking typeof and returns object as an object type
 runTest("Task 1", function () {
   const myObject = {};
 
-  check(FILL_ME_IN).isEqualTo(typeof myObject);
+  check("object").isEqualTo(typeof myObject);
 });
 
 // once you have passed the test above, change skipTest below to runTest so you are able to run the next test
-skipTest("Task 2", function () {
+runTest("Task 2", function () {
   const father = {
     firstName: "Michael",
     lastName: "Bluth",
     age: 33,
   };
 
-  check(FILL_ME_IN).isEqualTo(father.firstName);
-  check(FILL_ME_IN).isEqualTo(father.lastName);
-  check(FILL_ME_IN).isEqualTo(father.firstname);
-  check(FILL_ME_IN).isEqualTo(father.age);
+  check("Michael").isEqualTo(father.firstName);
+  check("Bluth").isEqualTo(father.lastName);
+  check().isEqualTo(father.firstname);
+  check(33).isEqualTo(father.age);
 });
 
 // once you have passed the test above, change skipTest below to runTest so you are able to run the next test
-skipTest("Task 3", function () {
+runTest("Task 3", function () {
   const son = {};
   son.name = "George Michael";
   son.lastname = "Bluth";
   son.age = 16;
   son.jobs = ["Frozen Banana Salesman", "CEO of Fakeblock"];
 
-  check(FILL_ME_IN).isEqualTo(son.name);
-  check(FILL_ME_IN).isEqualTo(typeof son.jobs);
-  check(FILL_ME_IN).isEqualTo(son.jobs[1][5]);
+  check("George Michael").isEqualTo(son.name);
+  check("object").isEqualTo(typeof son.jobs);
+  //Second array element 5th element position
+  check("f").isEqualTo(son.jobs[1][5]);
 });
 
 // once you have passed the test above, change skipTest below to runTest so you are able to run the next test
-skipTest("Task 4", function () {
+runTest("Task 4", function () {
   const starWars = {
     episode4: "A New Hope",
     episode5: "Empire Strikes Back",
     episode6: "Return of the Jedi",
     episode7: "The Force Awakens",
   };
+  //key reference is created by the returned value of the key name "episode" concatenated with
+  //the result of 10 -4 thus episode 6
   const worstOne = starWars["episode" + (10 - 4)];
 
-  check(FILL_ME_IN).isEqualTo(worstOne);
-  check(starWars["episode7"]).isEqualTo(FILL_ME_IN);
+  check("Return of the Jedi").isEqualTo(worstOne);
+  check(starWars["episode7"]).isEqualTo("The Force Awakens");
 });
 
 // once you have passed the test above, change skipTest below to runTest so you are able to run the next test
-skipTest("Task 5", function () {
+runTest("Task 5", function () {
   const brotherInLaw = {
     name: "Tobias",
     lastname: "Funke",
     job: "therapist",
   };
 
-  check(FILL_ME_IN).isEqualTo(brotherInLaw.job);
+  check("therapist").isEqualTo(brotherInLaw.job);
   brotherInLaw.job = "actor";
-  check(FILL_ME_IN).isEqualTo(brotherInLaw.job);
+  check("actor").isEqualTo(brotherInLaw.job);
 
   delete brotherInLaw.job;
-  check(FILL_ME_IN).isEqualTo(brotherInLaw.job);
+  check().isEqualTo(brotherInLaw.job);
 });
 
 // once you have passed the test above, change skipTest below to runTest so you are able to run the next test
-skipTest("Task 6", function () {
+runTest("Task 6", function () {
   const bluthFamily = {
     father: {
       name: "George",
@@ -76,9 +79,11 @@ skipTest("Task 6", function () {
     daughters: [{ name: "Lindsay" }],
   };
 
-  check(FILL_ME_IN).isEqualTo(bluthFamily.father.name);
-  check(bluthFamily.mother.name).isEqualTo(FILL_ME_IN);
-  check(bluthFamily.daughters[FILL_ME_IN].FILL_ME_IN).isEqualTo("Lindsay");
+  check("George").isEqualTo(bluthFamily.father.name);
+  check(bluthFamily.mother.name).isEqualTo("Lucille");
+  //Here we are accessing the daugthers key and then the first element of the array, and 
+  //explicitly calling the name key. Here we have an object nested inside an array
+  check(bluthFamily.daughters[0].name).isEqualTo("Lindsay");
 });
 
 var FILL_ME_IN;
